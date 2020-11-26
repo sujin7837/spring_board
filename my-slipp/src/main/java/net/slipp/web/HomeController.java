ccf34671ab6def175a48f7831add6dc949b8f2c2
sujin7837 <31465594+sujin7837@users.noreply.github.com>
2020-11-26 16:33:34 +0900

    질문 목록 기능 구현 완료

--- my-slipp/src/main/java/net/slipp/web/HomeController.java
+++ my-slipp/src/main/java/net/slipp/web/HomeController.java
@@ -1,12 +1,20 @@
 package net.slipp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import net.slipp.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private QuestionRepository questionRepository;	//데이터베이스로부터 질문의 목록 받아오기
+	
 	@GetMapping("")
-	public String name() {
+	public String name(Model model) {
+		model.addAttribute("questions", questionRepository.findAll());	//questions라는 이름으로 model의 데이터를 담아서 전달
 		return "index";
 	}
 }
